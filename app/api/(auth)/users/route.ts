b278f0730d333a1597086d475e92ff4a113cf72d
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-06-29 17:45:39 +0200

    fix env, update get , add oApi

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,5 +1,16 @@
 import {NextResponse} from "next/server";
+import connect from "@/lib/db";
+import User from "@/lib/models/users";
 
-export const GET = () => {
-    return new NextResponse('this is response');
-}
+export const GET = async () => {
+
+    try {
+        await connect()
+        const users = await User.find();
+        return new NextResponse(JSON.stringify(users), {status: 200})
+    } catch (err: any) {
+        return new NextResponse('Error in feetchin users ' + err.message, { status: 500});
+    }
+
+};
+
