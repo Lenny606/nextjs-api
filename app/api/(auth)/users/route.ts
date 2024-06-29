62fa3902a965510d64f7ec49f8b93cc2991a8928
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-06-29 18:26:36 +0200

    POST user

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -9,8 +9,20 @@ export const GET = async () => {
         const users = await User.find();
         return new NextResponse(JSON.stringify(users), {status: 200})
     } catch (err: any) {
-        return new NextResponse('Error in feetchin users ' + err.message, { status: 500});
+        return new NextResponse('Error in fetchingg users ' + err.message, {status: 500});
     }
 
 };
+export const POST = async (req: Request, res: Response) => {
+    try {
+        const body = await req.json();
+        await connect()
+        const user = new User(body)
+        await user.save();
+
+        return new NextResponse(JSON.stringify({message: "user created", data: user}), {status: 201})
+    } catch (err: any) {
+        return new NextResponse('Error in creating users ' + err.message, {status: 500});
+    }
+}
 
